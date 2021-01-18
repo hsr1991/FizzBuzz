2ab539e07bd70bccef843861c652960d9ec908d2
hsr1991 <hsr1991@hotmail.co.uk>
2021-01-18 22:20:50 +0000

    fizz and buzz for multiples of 3 and 5 respectively, 'FizzBuzz' for both 3&&5

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -1,10 +1,14 @@
 function FizzBuzz() {
     for(var i = 1; i<=100; i++) { 
-    if (Number.isInteger(i/3)) {
+    if (Number.isInteger(i/3) && Number.isInteger(i/5)) {
+        console.log('FizzBuzz') 
+    } if (Number.isInteger(i/3)) {
     console.log('Fizz')
-    } else{
-    console.log(i)
-    }
-    }
+    } if (Number.isInteger(i/5)) {
+    console.log('Buzz')
+    } else { 
+        console.log(i)
+      }
+     }
     }
     FizzBuzz()
