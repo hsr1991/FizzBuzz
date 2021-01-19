9d45f0c79d67a59d17bbd632af803c6d4d40a6ca
hsr1991 <hsr1991@hotmail.co.uk>
2021-01-19 11:00:16 +0000

    Multiples of 11 bong

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -1,12 +1,14 @@
-function FizzBuzz() { 
+    function FizzBuzz() {
     for(var i = 1; i<=100; i++) {
-    if (Number.isInteger(i/3) && Number.isInteger(i/5)) {
+    if (Number.isInteger(i/11)) {
+            console.log('Bong')
+    } if (Number.isInteger(i/3) && Number.isInteger(i/5)) {
         console.log('FizzBuzz') 
-    }  if (Number.isInteger(i/3) && Number.isInteger(i/7)) {
+    } if (Number.isInteger(i/3) && Number.isInteger(i/7)) {
         console.log('FizzBang') 
     } if (Number.isInteger(i/7) && Number.isInteger(i/5)) {
         console.log('FizzBang') 
-     } if (Number.isInteger(i/7)) {
+     } else if (Number.isInteger(i/7)) {
             console.log('Bang')
       } if (Number.isInteger(i/3)) {
     console.log('Fizz')
