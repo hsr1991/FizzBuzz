e0ae5def8b82db77f1c697bb7b42c076b878879f
hsr1991 <hsr1991@hotmail.co.uk>
2021-01-19 10:47:43 +0000

    FizzBang

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -1,8 +1,14 @@
-function FizzBuzz() {
-    for(var i = 1; i<=100; i++) { 
+function FizzBuzz() { 
+    for(var i = 1; i<=100; i++) {
     if (Number.isInteger(i/3) && Number.isInteger(i/5)) {
         console.log('FizzBuzz') 
-    } if (Number.isInteger(i/3)) {
+    }  if (Number.isInteger(i/3) && Number.isInteger(i/7)) {
+        console.log('FizzBang') 
+    } if (Number.isInteger(i/7) && Number.isInteger(i/5)) {
+        console.log('FizzBang') 
+     } if (Number.isInteger(i/7)) {
+            console.log('Bang')
+      } if (Number.isInteger(i/3)) {
     console.log('Fizz')
     } if (Number.isInteger(i/5)) {
     console.log('Buzz')
